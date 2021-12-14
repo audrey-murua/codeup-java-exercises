b30f5e708c79bb731c7e5e35bdf5180707bef916
audrey murua <audrey.murua1@gmail.com>
2021-12-14 12:20:49 -0600

    completed first Java program

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+//public class HelloWorld {
+//    public static void main (String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+
+
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.print("Hello ");
+            System.out.print("World!");
+        }
+    }
