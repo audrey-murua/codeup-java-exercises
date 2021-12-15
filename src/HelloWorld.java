0e35d028169de76c6683dd8a2bcaabadfb65afec
audrey murua <audrey.murua1@gmail.com>
2021-12-15 10:48:23 -0600

    syntax, types and variables exercise done, warmup 12-15-21

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,89 @@
-//public class HelloWorld {
-//    public static void main (String[] args) {
-//        System.out.println("Hello World!");
-//    }
-//}
 
 
     public class HelloWorld {
         public static void main(String[] args) {
+            //        System.out.println("Hello World!");
             System.out.print("Hello ");
             System.out.print("World!");
+
+            int myFavoriteNumber = 12;
+            System.out.println(myFavoriteNumber);
+
+            String myString = "Hello Codeup!";
+            System.out.println(myString);
+
+//
+//            myString = "H";
+//            System.out.print(myString);
+
+
+//            myString = 3.14159;
+//            System.out.print(myString);
+
+//            long myNumber;
+//            System.out.print(myNumber);
+
+//            myNumber = 3.14;
+//            System.out.print(myNumber);
+
+//            myNumber = 123L;
+//            System.out.print(myNumber);
+//
+//            myNumber = 123;
+//            System.out.println(myNumber);
+
+//            float myNumber = 3.14f;
+//            System.out.println(myNumber);
+
+            float myNumber = (float)3.14;
+            System.out.println(myNumber);
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+//          the result is 5 and then 6. This is using post increment, which takes the value prints it. then it will increment it and post the next number up.
+
+            // the result is 6 and 6. This is using pre increment, it takes the value and increments it, then it will print.
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
+
+//            class = "hello";
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+//            System.out.println(theNumberThree);
+//            System.out.println(o);
+//            System.out.println(three);
+
+
+//            int three = (int) "three";
+//            System.out.println(three);
+
+//             run and compile errors were observed
+
+//            int x = 4;
+//            x = x + 5;
+            //answer
+//            x += 5;
+
+//            int x = 3;
+//            int y = 4;
+//            y = y * x;
+            //answer
+//            y *= x;
+
+//            int x = 10;
+//            int y = 2;
+//            x = x / y;
+//            y = y - x;
+            //answer
+//            x /= y;
+//            y -= x;
+
+            // If the number is too large for the variable, it will give you a compile error. Smaller values than the integer can hold will still print. If you increment past the capacity, you'll get integer overflow.
+
         }
     }
+
