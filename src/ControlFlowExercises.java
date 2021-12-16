0f2a73b6963c0a865d32b6e025ec9cc754acb8a7
audrey murua <audrey.murua1@gmail.com>
2021-12-16 14:52:40 -0600

    1c-1b1 done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -63,7 +63,11 @@ public class ControlFlowExercises {
                 System.out.printf("%s ", c);
             }
 
-
+//            Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//
+            for(int d = 0; d <= 100; d+=2) {
+                System.out.println(d);
+            }
 
 
 
