b1f34feb01676e947101b54463e086556b9c282d
audrey murua <audrey.murua1@gmail.com>
2021-12-16 13:59:38 -0600

    first question done

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            //question 1.a
+
+            int i = 5;
+//            Create a while loop that runs so long as i is less than or equal to 15
+//            Each loop iteration, output the current value of i, then increment i by one.
+//
+//            Your output should look like this:
+//
+//            5 6 7 8 9 10 11 12 13 14 15
+
+            while ( i <= 15 ) {
+                System.out.printf("%s ", i);
+                i++;
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
